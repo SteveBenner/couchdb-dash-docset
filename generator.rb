e4ffe0473d2d1e27d3cf40a657e4464cd52fec86
Stephen Benner <sbenner9@gmail.com>
2013-11-06 00:03:18 -0800

    changed license to MIT from GPLv3

--- generator.rb
+++ generator.rb
@@ -3,22 +3,6 @@
 # Dash docset generator for Apache CouchDB 1.4
 # This is simply a tool for generating Dash docsets from HTML documentation of any kind. It is configured for CouchDB.
 
-# LICENSE:
-# Copyright (C) 2013 Stephen C. Benner
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 require 'fileutils'
 require 'open-uri' # built-in Ruby module that encapsulates an HTTP request within the 'open' command
 require 'sqlite3'
